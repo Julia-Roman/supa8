7f9df6647d2b1901c033f791b935285cd93e170d
Supa <36031171+0Supa@users.noreply.github.com>
2025-11-11 05:31:50 +0200

    add deps version to ping

--- fun/cmds/ping.go
+++ fun/cmds/ping.go
@@ -2,7 +2,7 @@ package fun
 
 import (
 	"fmt"
-	"math/rand"
+	"os/exec"
 	"runtime"
 	"time"
 
@@ -11,6 +11,21 @@ import (
 	. "github.com/0supa/supa8/fun/api/twitch"
 	"github.com/gempir/go-twitch-irc/v4"
 )
+
+func runCommands(cmds [][]string) (res []string, err error) {
+	for _, c := range cmds {
+		if len(c) == 0 {
+			continue
+		}
+		cmd := exec.Command(c[0], c[1:]...)
+		b, err := cmd.CombinedOutput()
+		if err != nil {
+			return res, fmt.Errorf("command %v failed: %w; output: %s", c[0], err, string(b))
+		}
+		res = append(res, string(b))
+	}
+	return
+}
 
 func init() {
 	Fun.Register(&Cmd{
@@ -23,11 +38,26 @@ func init() {
 			var mem runtime.MemStats
 			runtime.ReadMemStats(&mem)
 
-			e := []string{":3", ":x", "c:", ":$", ">.<", "^.^", "^o^"}
-			_, err = Say(m.RoomID, fmt.Sprintf("%s %vms, %s, %vMiB, up:%s, channels:%v, blocked:%v",
-				e[rand.Intn(len(e))],
+			res, err := runCommands([][]string{
+				{"git", "rev-parse", "--short", "HEAD"},
+				{"yt-dlp", "--version"},
+				{"ffmpeg", "-version"},
+				{"zbarimg", "--version"},
+			})
+			if err != nil {
+				return
+			}
+
+			var ffmpegVer string
+			fmt.Sscanf(res[2], "ffmpeg version %s", &ffmpegVer)
+
+			_, err = Say(m.RoomID, fmt.Sprintf("pong!! %vms, commit:%s, %s, yt-dlp:%s, ffmpeg:%s, zbar:%s, %vMiB, up:%s, channels:%v, blocked:%v",
 				time.Since(m.Time).Milliseconds(),
+				res[0],
 				runtime.Version(),
+				res[1],
+				ffmpegVer,
+				res[3],
 				mem.Alloc/1024/1024,
 				time.Since(InitTime).Truncate(time.Second),
 				len(config.Meta.Channels),
